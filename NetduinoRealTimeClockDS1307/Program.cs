c74abb3fa9d018632030b4fdd0395983684a3b23
aaronamm <theeranitp@gmail.com>
2019-01-24 00:29:50 +0700

    Improve Date format with NetMf.CommonExtensions string format

--- NetduinoRealTimeClockDS1307/Program.cs
+++ NetduinoRealTimeClockDS1307/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.SPOT;
+using NetMf.CommonExtensions;
 using System.Threading;
 using Toolbox.NETMF.Hardware;
 
@@ -9,25 +10,21 @@ namespace NetduinoRealTimeClockDS1307
         public static void Main()
         {
             var ds1307 = new DS1307();
-            // To set the time to DS1307 board, comment out 
-            // ds1307.SetTime(Year: 2018, Month: 12, Day: 24, Hour: 20, Minute: 28, Second: 0);
+            //Uncomment the following line to set the time to DS1307 board
+            //ds1307.SetTime(Year: 2018, Month: 01, Day: 23, Hour: 16, Minute: 59, Second: 0);
             while (true)
             {
                 var time = ds1307.GetTime();
-                Debug.Print("Netduino Time: " + time.ToString());
+                var formattedDate = StringUtility.Format(
+                    "Netduino Time in UTC: {0}",
+                    time.ToString("yyyy-MM-ddTHH:mm:ssZ")
+                );
+                Debug.Print(formattedDate);
+
+                //sleep for 5 seconds
                 Thread.Sleep(5000);
             }
         }
-    }
-}
-
-namespace System.Diagnostics
-{
-    public enum DebuggerBrowsableState
-    {
-        Collapsed,
-        Never,
-        RootHidden
     }
 }
 
