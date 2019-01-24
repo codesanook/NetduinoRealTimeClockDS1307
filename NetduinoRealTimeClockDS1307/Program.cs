1df543e101ca8486b145623ec40cad071191e017
aaronamm <theeranitp@gmail.com>
2019-01-24 20:14:33 +0700

    Format  setTime method

--- NetduinoRealTimeClockDS1307/Program.cs
+++ NetduinoRealTimeClockDS1307/Program.cs
@@ -10,8 +10,16 @@ namespace NetduinoRealTimeClockDS1307
         public static void Main()
         {
             var ds1307 = new DS1307();
-            //Uncomment the following line to set the time to DS1307 board
-            //ds1307.SetTime(Year: 2018, Month: 01, Day: 23, Hour: 16, Minute: 59, Second: 0);
+            //Comment the following line to not set the time to DS1307 board again
+            ds1307.SetTime(
+                Year: 2018,
+                Month: 01,
+                Day: 23,
+                Hour: 16,
+                Minute: 59,
+                Second: 0
+            );
+
             while (true)
             {
                 var time = ds1307.GetTime();
@@ -19,8 +27,8 @@ namespace NetduinoRealTimeClockDS1307
                     "Netduino Time in UTC: {0}",
                     time.ToString("yyyy-MM-ddTHH:mm:ssZ")
                 );
-                Debug.Print(formattedDate);
 
+                Debug.Print(formattedDate);
                 //sleep for 5 seconds
                 Thread.Sleep(5000);
             }
